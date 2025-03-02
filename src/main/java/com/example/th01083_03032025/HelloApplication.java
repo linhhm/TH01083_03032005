bff228653000a816731ece0cb16e33997d05dfb7
linhhm <hmailinh9@gmail.com>
2025-03-03 01:55:24 +0700

    first commit

---
+++ src/main/java/com/example/th01083_03032025/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.th01083_03032025;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
